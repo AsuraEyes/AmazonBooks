79cb05ae86e3198b3e036594876e0b9c99e958b6
Timothy Engkar <56295945+AsuraEyes@users.noreply.github.com>
2023-02-10 14:03:30 +0100

    modified entities

--- create_table.sql
+++ create_table.sql
@@ -5,19 +5,19 @@ SET SCHEMA 'amazon_books';
 DROP TABLE IF EXISTS language;
 CREATE TABLE language (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) NOT NULL
+    name VARCHAR (20) NOT NULL
 );
 
 DROP TABLE IF EXISTS format;
 CREATE TABLE format (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) NOT NULL
+    name VARCHAR (20) NOT NULL
 );
 
 DROP TABLE IF EXISTS category;
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) NOT NULL
+    name VARCHAR (50) NOT NULL
 );
 
 DROP TABLE IF EXISTS sub_category;
@@ -39,7 +39,7 @@ CREATE TABLE character (
 DROP TABLE IF EXISTS genre;
 CREATE TABLE genre (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) NOT NULL,
+    name VARCHAR (20) NOT NULL,
     ca_id INT NOT NULL,
     FOREIGN KEY (ca_id) REFERENCES category (id)
 );
@@ -47,7 +47,7 @@ CREATE TABLE genre (
 DROP TABLE IF EXISTS country;
 CREATE TABLE country (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) NOT NULL
+    name VARCHAR (75) NOT NULL
 );
 
 DROP TABLE IF EXISTS customer;
@@ -80,8 +80,8 @@ CREATE TABLE author (
 
 DROP TABLE IF EXISTS book;
 CREATE TABLE book (
-    isbn_10 VARCHAR NOT NULL UNIQUE,
-    isbn_13 VARCHAR NOT NULL UNIQUE,
+    isbn_10 VARCHAR (10) NOT NULL UNIQUE,
+    isbn_13 VARCHAR (14) NOT NULL UNIQUE,
     title VARCHAR (100) NOT NULL,
     description VARCHAR NOT NULL,
     pages INT NOT NULL,
@@ -102,8 +102,8 @@ CREATE TABLE book (
 DROP TABLE IF EXISTS book_author;
 CREATE TABLE book_author (
     a_id INT NOT NULL,
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
+    isbn_10 VARCHAR (10) NOT NULL,
+    isbn_13 VARCHAR (14) NOT NULL,
     PRIMARY KEY (a_id, ISBN_10, ISBN_13),
     FOREIGN KEY (a_id) REFERENCES author (id),
     FOREIGN KEY (ISBN_10) REFERENCES book (ISBN_10),
@@ -112,8 +112,8 @@ CREATE TABLE book_author (
 
 DROP TABLE IF EXISTS book_genre;
 CREATE TABLE book_genre (
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
+    isbn_10 VARCHAR (10) NOT NULL,
+    isbn_13 VARCHAR (14) NOT NULL,
     g_id INT NOT NULL,
     PRIMARY KEY (isbn_10, isbn_13, g_id),
     FOREIGN KEY (isbn_10) REFERENCES book (isbn_10),
@@ -123,8 +123,8 @@ CREATE TABLE book_genre (
 
 DROP TABLE IF EXISTS book_character;
 CREATE TABLE book_character (
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
+    isbn_10 VARCHAR (10) NOT NULL,
+    isbn_13 VARCHAR (14) NOT NULL,
     ch_id INT NOT NULL,
     PRIMARY KEY (isbn_10, isbn_13, ch_id),
     FOREIGN KEY (isbn_10) REFERENCES book (isbn_10),
@@ -134,8 +134,8 @@ CREATE TABLE book_character (
 
 DROP TABLE IF EXISTS book_category;
 CREATE TABLE book_category (
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
+    isbn_10 VARCHAR (10) NOT NULL,
+    isbn_13 VARCHAR (14) NOT NULL,
     ca_id INT NOT NULL,
     PRIMARY KEY (isbn_10, isbn_13, ca_id),
     FOREIGN KEY (isbn_10) REFERENCES book (isbn_10),
@@ -156,8 +156,8 @@ CREATE TABLE "order" (
 
 DROP TABLE IF EXISTS book_order;
 CREATE TABLE book_order (
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
+    isbn_10 VARCHAR (10) NOT NULL,
+    isbn_13 VARCHAR (14) NOT NULL,
     o_id INT NOT NULL,
     quantity INT NOT NULL,
     unit_price INT NOT NULL,
@@ -168,16 +168,16 @@ CREATE TABLE book_order (
     FOREIGN KEY (o_id) REFERENCES "order" (id)
 );
 
-DROP TABLE IF EXISTS amazon_order;
-CREATE TABLE amazon_order (
-    isbn_10 VARCHAR NOT NULL,
-    isbn_13 VARCHAR NOT NULL,
-    c_id INT NOT NULL,
-    o_id INT NOT NULL,
-    total_price DECIMAL NOT NULL,
-    PRIMARY KEY (ISBN_10, ISBN_13, c_id, o_id),
-    FOREIGN KEY (ISBN_10) REFERENCES book (ISBN_10),
-    FOREIGN KEY (ISBN_13) REFERENCES book (ISBN_13),
-    FOREIGN KEY (c_id) REFERENCES customer (id),
-    FOREIGN KEY (o_id) REFERENCES "order" (id)
-);
+-- DROP TABLE IF EXISTS amazon_order;
+-- CREATE TABLE amazon_order (
+--     isbn_10 VARCHAR (10) NOT NULL,
+--     isbn_13 VARCHAR (14) NOT NULL,
+--     c_id INT NOT NULL,
+--     o_id INT NOT NULL,
+--     total_price DECIMAL NOT NULL,
+--     PRIMARY KEY (ISBN_10, ISBN_13, c_id, o_id),
+--     FOREIGN KEY (ISBN_10) REFERENCES book (ISBN_10),
+--     FOREIGN KEY (ISBN_13) REFERENCES book (ISBN_13),
+--     FOREIGN KEY (c_id) REFERENCES customer (id),
+--     FOREIGN KEY (o_id) REFERENCES "order" (id)
+-- );
